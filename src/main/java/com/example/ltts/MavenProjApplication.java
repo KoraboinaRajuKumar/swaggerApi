9f98d05f2c7a9d9b0bf3de0b8d834a47cdda4aca
unknown <rajujava0355@gmail.com.com>
2022-12-08 10:43:45 +0530

    first commit swagger

---
+++ src/main/java/com/example/ltts/MavenProjApplication.java
@@ -0,0 +1,15 @@
+package com.example.ltts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenProjApplication.class, args);
+		
+		System.out.println("koraboina raju kumar first application.........!");
+	}
+
+}
---
+++ src/test/java/com/example/ltts/MavenProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ltts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
